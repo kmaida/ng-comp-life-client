2880ce4fc56000f4b909c30a040bcfd076420f56
Kim Maida <kim.maida@auth0.com>
2018-04-03 09:32:09 -0400

    FIX path to component in spec file

--- src/app/pages/afterviewinit/afterviewinit.component.spec.ts
+++ src/app/pages/afterviewinit/afterviewinit.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AfterviewinitComponent } from './dinos.component';
+import { AfterviewinitComponent } from './afterviewinit.component';
 
 describe('AfterviewinitComponent', () => {
   let component: AfterviewinitComponent;

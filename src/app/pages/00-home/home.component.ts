804be86b03fae93908091754a28c11d8fc2438f6
Kim Maida <kim.maida@auth0.com>
2018-04-09 15:05:57 -0400

    Update some very minor styling and emojis

--- src/app/pages/00-home/home.component.ts
+++ src/app/pages/00-home/home.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: []
+  styles: [`
+    a:hover { text-decoration: none }
+  `]
 })
 export class HomeComponent implements OnInit {
 
